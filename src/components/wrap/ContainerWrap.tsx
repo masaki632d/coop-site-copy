b5d60cfa83e05b57f822d9b402c2685a8e7adfd6
koumura <masaki632d@gmail.com>
2022-10-23 23:51:17 +0900

    add: tailwindCSS

--- src/components/wrap/ContainerWrap.tsx
+++ src/components/wrap/ContainerWrap.tsx
@@ -1,12 +1,10 @@
 import { FC } from 'react'
 import { ReactNode } from 'react'
 
-import styles from '@styles/Home.module.css'
-
 export interface ContainerWrapProps {
   children: ReactNode
 }
 
 export const ContainerWrap: FC<ContainerWrapProps> = ({ children }) => (
-  <div className={styles.container}>{children}</div>
+  <div className="px-8 py-0">{children}</div>
 )
