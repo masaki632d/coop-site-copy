a5c039f83e44bf5b9cc7d7452898b508386bae1f
koumura <masaki632d@gmail.com>
2022-11-16 23:11:13 +0900

    コンポーネントのリファクタリング

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,32 +4,30 @@ import { FC } from 'react'
 
 import vercelImage from '@public_images/vercel.svg'
 
-export const Header: FC = () => {
-  return (
-    <header className="flex h-16 w-full items-center justify-between bg-white px-4 lg:h-24 lg:px-20">
-      <Link href="/">
-        <a>
-          <Image src={vercelImage} width={72} height={16} alt="ロゴ画像" />
-        </a>
-      </Link>
+export const Header: FC = () => (
+  <header className="flex h-16 w-full items-center justify-between bg-white px-4 lg:h-24 lg:px-20">
+    <Link href="/">
+      <a>
+        <Image src={vercelImage} width={72} height={16} alt="ロゴ画像" />
+      </a>
+    </Link>
 
-      <nav className="hidden items-center gap-6 text-base font-bold lg:flex">
-        <Link href="/about/">
-          <a>About</a>
-        </Link>
-        <Link href="/curry/">
-          <a>Curry</a>
-        </Link>
-        <Link href="/">
-          <a>Work</a>
-        </Link>
-        <Link href="/">
-          <a>Skill</a>
-        </Link>
-        <Link href="/">
-          <a>Contact</a>
-        </Link>
-      </nav>
-    </header>
-  )
-}
+    <nav className="hidden items-center gap-6 text-base font-bold lg:flex">
+      <Link href="/about/">
+        <a>About</a>
+      </Link>
+      <Link href="/curry/">
+        <a>Curry</a>
+      </Link>
+      <Link href="/">
+        <a>Work</a>
+      </Link>
+      <Link href="/">
+        <a>Skill</a>
+      </Link>
+      <Link href="/">
+        <a>Contact</a>
+      </Link>
+    </nav>
+  </header>
+)
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,19 +1,17 @@
 import Link from 'next/link'
 import { FC } from 'react'
 
-export const Navbar: FC = () => {
-  return (
-    <ul>
-      <li>
-        <Link href="/">
-          <a>Home</a>
-        </Link>
-      </li>
-      <li>
-        <Link href="/about">
-          <a>About</a>
-        </Link>
-      </li>
-    </ul>
-  )
-}
+export const Navbar: FC = () => (
+  <ul>
+    <li>
+      <Link href="/">
+        <a>Home</a>
+      </Link>
+    </li>
+    <li>
+      <Link href="/about">
+        <a>About</a>
+      </Link>
+    </li>
+  </ul>
+)
